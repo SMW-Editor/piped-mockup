29dba0573847ba812310bf14347c73ddcc728bd1
Pixelprizm <pixelprizm@gmail.com>
2024-09-12 18:57:06 -0700

    copy egui hello world example sans image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Piped Mockup",
+        options,
+        Box::new(|_| Ok(Box::<Model>::default())),
+    )
+}
+
+struct Model {
+    name: String,
+    age: u32,
+}
+
+impl Default for Model {
+    fn default() -> Self {
+        Self {
+            name: "Pixel".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for Model {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
 }
